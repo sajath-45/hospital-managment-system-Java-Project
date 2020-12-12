420a2af2ed9c2207c929910528a3e18be8e9dcf2
lathu0928 <lathu0928@users.noreply.github.com>
2020-12-12 08:39:43 -0800

    added user log report, patient credential report and appointmet report

---
+++ hospital managment system/src/hospital/managment/system/userLogReport.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author siva lathu
+ */
+public class userLogReport {
+    
+    private ArrayList<UserLogin> userLoginList;
+    private String strDate;
+    private String strUserType;
+    
+    public userLogReport(ArrayList<UserLogin> userLogInList, String Date, String userType){
+        setUserLogIn(userLogInList);
+        setStrDate(Date);
+        setStrUserType(userType);
+    }
+    
+    public ArrayList<UserLogin> getUserLogInList(){
+        return userLoginList;
+    }
+    public String getStrDate(){
+        return strDate;
+    }
+    public String getStrUserType(){
+        return strUserType;
+    }
+    
+    public void setUserLogIn(ArrayList<UserLogin> userLogInList) {
+        this.userLoginList=userLogInList;
+    }
+    public void setStrDate(String date) {
+        this.strDate=date;
+    }
+    public void setStrUserType(String userType) {
+        this.strUserType=userType;
+    }
+    
+}
